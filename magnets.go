3cd691ec09f69a91b67be860900100e5feb8a495
Erik Bryant <erikbryantology@gmail.com>
2020-07-03 13:56:56 -0700

    Simpler file handling

--- magnets.go
+++ magnets.go
@@ -1,32 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/erikbryant/magnets/magnets"
 	"github.com/erikbryant/magnets/solver"
 	"math/rand"
 	"os"
+	"strings"
+	"time"
 )
+
+func append(file, content string) {
+	f, err := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer f.Close()
+
+	_, err = f.WriteString(content + "\n")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
 
 // createCorups creates games and tries to solve them. The ones it can solve it writes to
 // one file and the ones it cannot solve it writes to another file.
 func createCorpus() {
-	s, err := os.Create("solved")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer s.Close()
-
-	u, err := os.Create("unsolved")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer u.Close()
-
 	games := 0
 
 	for solved := 0; solved < 1000000; {
@@ -42,13 +45,11 @@ func createCorpus() {
 			return
 		}
 
-		serial = fmt.Sprintf("%s\n", serial)
-
 		if game.Solved() {
 			solved++
-			_, err = s.WriteString(serial)
+			append("solved", serial)
 		} else {
-			_, err = u.WriteString(serial)
+			append("unsolved", serial)
 		}
 		if err != nil {
 			fmt.Println(err)
@@ -93,12 +94,14 @@ func deserializer(s string) {
 		return
 	}
 
+	fmt.Println("Solutions:", game.CountSolutions(0, 0))
+
 	solver.Solve(game)
 	if game.Solved() {
-		fmt.Println("Solved!")
-	} else {
-		fmt.Println("Could not solve:", s)
+		fmt.Println("Solved!", s)
 		game.Print()
+	} else {
+		fmt.Println("Could not solve!", s)
 	}
 }
 
@@ -116,8 +119,65 @@ func solvable(width, height int) {
 	}
 }
 
-func main() {
-	// createCorpus()
+// helper runs solver tests against a given file.
+func helper(file string, expected bool) {
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Printf("Unable to open testcases %s %s\n", file, err)
+	}
 
-	stressTest()
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		testCase := scanner.Text()
+
+		testCase = strings.TrimSpace(testCase)
+
+		if len(testCase) == 0 {
+			continue
+		}
+
+		if strings.HasPrefix(testCase, "//") {
+			continue
+		}
+
+		game, ok := magnets.Deserialize(testCase)
+		if !ok {
+			fmt.Printf("ERROR: Unable to deserialize %s\n", testCase)
+		}
+
+		solutions := game.CountSolutions(0, 0)
+		if solutions == 1 {
+			append("test_success", testCase)
+		} else {
+			append("test_fail", testCase)
+		}
+
+		// solver.Solve(game)
+		// if game.Solved() != expected {
+		// 	fmt.Println("ERROR: For %s expected solved to be %t", testCase, expected)
+		// }
+	}
+}
+
+func testSolve() {
+	helper("./solver/test_solve.txt", true)
+	// helper("./solver/test_solve_fail.txt", false)
+}
+
+func main() {
+	start := time.Now()
+
+	// createCorpus()
+	// stressTest()
+	testSolve()
+
+	// deserializer("1x2:1,10,1,01,TB")
+	// deserializer("1x3:1,100,1,010,TB*")
+	// deserializer("2x2:11,11,11,11,TTBB")
+	// deserializer("6x6:233322,123333,323232,033333,LRTLRTTTBTTBBBTBBTTTBTTBBBTBBTLRBLRB")
+	// deserializer("16x3:2121212121212120,878,1212121212121211,887,TTTLRTLRLRTTTTLRBBBLRBTTLRBBBBTTLRLRLRBBLRLRLRBB")
+
+	fmt.Println("testSolve took", time.Since(start))
 }
