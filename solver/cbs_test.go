9a033d069e3e3aa62c521a46f73f6f84f207ae78
Erik Bryant <erikbryantology@gmail.com>
2020-07-05 10:13:32 -0700

    Refactor solver module
    Make it easier to find which functions are solver rules and which are
    CBS manipulation tools.

---
+++ solver/cbs_test.go
@@ -0,0 +1,431 @@
+package solver
+
+import (
+	"github.com/erikbryant/magnets/common"
+	"github.com/erikbryant/magnets/magnets"
+	"testing"
+)
+
+func TestNewNoWall(t *testing.T) {
+	game, ok := magnets.Deserialize("5x4:02121,2112,20211,1212,LRLRTLRTTBLRBBTLRLRB")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	cbs := new(game)
+
+	answer := len(cbs)
+	if answer != 4 {
+		t.Errorf("ERROR: Expected 4, got %d", answer)
+	}
+	answer = len(cbs[0])
+	if answer != 5 {
+		t.Errorf("ERROR: Expected 5, got %d", answer)
+	}
+	answer = len(cbs[0][0])
+	if answer != 3 {
+		t.Errorf("ERROR: Expected 3, got %d", answer)
+	}
+
+	game, ok = magnets.Deserialize("5x5:03232,22222,12322,32221,LRLRTTLRTBBTTBTTBBTBBLRB*")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+}
+
+func TestNewWithWall(t *testing.T) {
+	game, ok := magnets.Deserialize("5x7:32323,3222211,33223,2312221,TLRTTBLRBBTTTLRBBBTTTLRBBBLRLR*LRLR")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	cbs := new(game)
+
+	answer := len(cbs)
+	if answer != 7 {
+		t.Errorf("ERROR: Expected 7, got %d", answer)
+	}
+	answer = len(cbs[0])
+	if answer != 5 {
+		t.Errorf("ERROR: Expected 5, got %d", answer)
+	}
+	answer = len(cbs[0][0])
+	if answer != 3 {
+		t.Errorf("ERROR: Expected 3, got %d", answer)
+	}
+	r := game.Guess.Get(6, 0)
+	if r != common.Wall {
+		t.Errorf("ERROR: Expected Wall, got %c", r)
+	}
+}
+
+func TestSetFrame(t *testing.T) {
+	testCases := []struct {
+		r        rune
+		expected rune
+	}{
+		{common.Positive, common.Negative},
+		{common.Negative, common.Positive},
+		{common.Neutral, common.Neutral},
+	}
+
+	game, ok := magnets.Deserialize("2x1:10,1,01,1,LR")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	cbs := new(game)
+
+	for _, testCase := range testCases {
+		dirty = false
+		cbs.setFrame(game, 0, 0, testCase.r)
+		answer := game.Guess.Get(0, 1)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: Expected '%c' got '%c'", testCase.expected, answer)
+		}
+		if !dirty {
+			t.Errorf("ERROR: Expected dirty = true, got dirty = %v", dirty)
+		}
+	}
+}
+
+func TestUnsetPossibility(t *testing.T) {
+	game, ok := magnets.Deserialize("2x1:10,1,01,1,LR")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	cbs := new(game)
+
+	answer := len(cbs[0][0])
+	if answer != 3 {
+		t.Errorf("ERROR: Expected 3, got %d", answer)
+	}
+
+	dirty = false
+	cbs.unsetPossibility(0, 0, common.Positive)
+	answer = len(cbs[0][0])
+	if answer != 2 {
+		t.Errorf("ERROR: Expected 2, got %d", answer)
+	}
+	if !dirty {
+		t.Errorf("ERROR: Expected dirty = true, got dirty = %v", dirty)
+	}
+
+	dirty = false
+	cbs.unsetPossibility(0, 0, common.Positive)
+	answer = len(cbs[0][0])
+	if answer != 2 {
+		t.Errorf("ERROR: Expected still to be 2, got %d", answer)
+	}
+	if dirty {
+		t.Errorf("ERROR: Expected dirty = false, got dirty = %v", dirty)
+	}
+
+	dirty = false
+	cbs.unsetPossibility(0, 0, common.Negative)
+	answer = len(cbs[0][0])
+	if answer != 1 {
+		t.Errorf("ERROR: Expected 1, got %d", answer)
+	}
+	if !dirty {
+		t.Errorf("ERROR: Expected dirty = true, got dirty = %v", dirty)
+	}
+
+	dirty = false
+	cbs.unsetPossibility(0, 0, common.Neutral)
+	answer = len(cbs[0][0])
+	if answer != 0 {
+		t.Errorf("ERROR: Expected 0, got %d", answer)
+	}
+	if !dirty {
+		t.Errorf("ERROR: Expected dirty = true, got dirty = %v", dirty)
+	}
+}
+
+func TestRowNeeds(t *testing.T) {
+	testCases := []struct {
+		row             int
+		expectedPos     int
+		expectedNeg     int
+		expectedNeutral int
+	}{
+		{0, 1, 2, 0},
+		{1, 2, 1, 0},
+		{2, 0, 1, 2},
+		{3, 2, 1, 0},
+	}
+
+	game, ok := magnets.Deserialize("3x4:212,1202,122,2111,TTTBBBLRTLRB")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	// Positive
+	for _, testCase := range testCases {
+		answer := rowNeeds(game, testCase.row, common.Positive)
+		if answer != testCase.expectedPos {
+			t.Errorf("ERROR: For row %d expected rowNeeds %d, got %d", testCase.row, testCase.expectedPos, answer)
+		}
+	}
+
+	// Negative
+	for _, testCase := range testCases {
+		answer := rowNeeds(game, testCase.row, common.Negative)
+		if answer != testCase.expectedNeg {
+			t.Errorf("ERROR: For row %d expected rowNeeds %d, got %d", testCase.row, testCase.expectedNeg, answer)
+		}
+	}
+
+	// Neutral
+	for _, testCase := range testCases {
+		answer := rowNeeds(game, testCase.row, common.Neutral)
+		if answer != testCase.expectedNeutral {
+			t.Errorf("ERROR: For row %d expected rowNeeds %d, got %d", testCase.row, testCase.expectedNeutral, answer)
+		}
+	}
+}
+
+func TestRowNeeds_PartiallySolved(t *testing.T) {
+	testCases := []struct {
+		row             int
+		expectedPos     int
+		expectedNeg     int
+		expectedNeutral int
+	}{
+		{0, 1, 2, 0},
+		{1, 2, 1, 0},
+		{2, 0, 1, 0},
+		{3, 1, 0, 0},
+	}
+
+	game, ok := magnets.Deserialize("3x4:212,1202,122,2111,TTTBBBLRTLRB")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	cbs := new(game)
+
+	// Solve a little bit of the board.
+	cbs.setFrame(game, 3, 0, common.Positive)
+	cbs.setFrame(game, 2, 0, common.Neutral)
+
+	// Positive
+	for _, testCase := range testCases {
+		answer := rowNeeds(game, testCase.row, common.Positive)
+		if answer != testCase.expectedPos {
+			t.Errorf("ERROR: For row %d expected rowNeeds %d, got %d", testCase.row, testCase.expectedPos, answer)
+		}
+	}
+
+	// Negative
+	for _, testCase := range testCases {
+		answer := rowNeeds(game, testCase.row, common.Negative)
+		if answer != testCase.expectedNeg {
+			t.Errorf("ERROR: For row %d expected rowNeeds %d, got %d", testCase.row, testCase.expectedNeg, answer)
+		}
+	}
+
+	// Neutral
+	for _, testCase := range testCases {
+		answer := rowNeeds(game, testCase.row, common.Neutral)
+		if answer != testCase.expectedNeutral {
+			t.Errorf("ERROR: For row %d expected rowNeeds %d, got %d", testCase.row, testCase.expectedNeutral, answer)
+		}
+	}
+}
+
+func TestColNeeds(t *testing.T) {
+	testCases := []struct {
+		col             int
+		expectedPos     int
+		expectedNeg     int
+		expectedNeutral int
+	}{
+		{0, 2, 1, 1},
+		{1, 1, 2, 1},
+		{2, 2, 2, 0},
+	}
+
+	game, ok := magnets.Deserialize("3x4:212,1202,122,2111,TTTBBBLRTLRB")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	// Positive
+	for _, testCase := range testCases {
+		answer := colNeeds(game, testCase.col, common.Positive)
+		if answer != testCase.expectedPos {
+			t.Errorf("ERROR: For col %d expected colNeeds %d, got %d", testCase.col, testCase.expectedPos, answer)
+		}
+	}
+
+	// Negative
+	for _, testCase := range testCases {
+		answer := colNeeds(game, testCase.col, common.Negative)
+		if answer != testCase.expectedNeg {
+			t.Errorf("ERROR: For col %d expected colNeeds %d, got %d", testCase.col, testCase.expectedNeg, answer)
+		}
+	}
+
+	// Neutral
+	for _, testCase := range testCases {
+		answer := colNeeds(game, testCase.col, common.Neutral)
+		if answer != testCase.expectedNeutral {
+			t.Errorf("ERROR: For col %d expected colNeeds %d, got %d", testCase.col, testCase.expectedNeutral, answer)
+		}
+	}
+}
+
+func TestColNeeds_PartiallySolved(t *testing.T) {
+	testCases := []struct {
+		col             int
+		expectedPos     int
+		expectedNeg     int
+		expectedNeutral int
+	}{
+		{0, 1, 1, 0},
+		{1, 1, 1, 0},
+		{2, 2, 2, 0},
+	}
+
+	game, ok := magnets.Deserialize("3x4:212,1202,122,2111,TTTBBBLRTLRB")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	cbs := new(game)
+
+	// Solve a little bit of the board.
+	cbs.setFrame(game, 3, 0, common.Positive)
+	cbs.setFrame(game, 2, 0, common.Neutral)
+
+	// Positive
+	for _, testCase := range testCases {
+		answer := colNeeds(game, testCase.col, common.Positive)
+		if answer != testCase.expectedPos {
+			t.Errorf("ERROR: For col %d expected colNeeds %d, got %d", testCase.col, testCase.expectedPos, answer)
+		}
+	}
+
+	// Negative
+	for _, testCase := range testCases {
+		answer := colNeeds(game, testCase.col, common.Negative)
+		if answer != testCase.expectedNeg {
+			t.Errorf("ERROR: For col %d expected colNeeds %d, got %d", testCase.col, testCase.expectedNeg, answer)
+		}
+	}
+
+	// Neutral
+	for _, testCase := range testCases {
+		answer := colNeeds(game, testCase.col, common.Neutral)
+		if answer != testCase.expectedNeutral {
+			t.Errorf("ERROR: For col %d expected colNeeds %d, got %d", testCase.col, testCase.expectedNeutral, answer)
+		}
+	}
+}
+
+func TestRowHasSpaceForTotal(t *testing.T) {
+	testCases := []struct {
+		row             int
+		expectedPos     int
+		expectedNeg     int
+		expectedNeutral int
+	}{
+		{0, 3, 3, 3},
+		{1, 3, 3, 3},
+		{2, 3, 3, 3},
+		{3, 3, 3, 3},
+	}
+
+	game, ok := magnets.Deserialize("3x4:212,1202,122,2111,TTTBBBLRTLRB")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	cbs := new(game)
+
+	// Positive
+	for _, testCase := range testCases {
+		answer := cbs.rowHasSpaceForTotal(game, testCase.row, common.Positive)
+		if answer != testCase.expectedPos {
+			t.Errorf("ERROR: For row %d expected rowHasSpaceForTotal %d, got %d", testCase.row, testCase.expectedPos, answer)
+		}
+	}
+
+	// Negative
+	for _, testCase := range testCases {
+		answer := cbs.rowHasSpaceForTotal(game, testCase.row, common.Negative)
+		if answer != testCase.expectedNeg {
+			t.Errorf("ERROR: For row %d expected rowHasSpaceForTotal %d, got %d", testCase.row, testCase.expectedNeg, answer)
+		}
+	}
+
+	// Neutral
+	for _, testCase := range testCases {
+		answer := cbs.rowHasSpaceForTotal(game, testCase.row, common.Neutral)
+		if answer != testCase.expectedNeutral {
+			t.Errorf("ERROR: For row %d expected rowHasSpaceForTotal %d, got %d", testCase.row, testCase.expectedNeutral, answer)
+		}
+	}
+}
+
+// func TestColHasSpaceForTotal(t *testing.T) {
+// 	t.Errorf("Not implemented")
+// }
+
+// func TestRowHasSpaceForRemaining(t *testing.T) {
+// 	t.Errorf("Not implemented")
+// }
+
+// func TestColHasSpaceForRemaining(t *testing.T) {
+// 	t.Errorf("Not implemented")
+// }
+
+func TestValidate(t *testing.T) {
+	// Test #1 - Invalid CBS state
+	game, ok := magnets.Deserialize("3x4:212,1202,122,2111,TTTBBBLRTLRB")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	cbs := new(game)
+
+	err := cbs.validate(game)
+	if err != nil {
+		t.Error("Error validating CBS", err)
+	}
+
+	game.Guess.Set(1, 1, common.Positive)
+	err = cbs.validate(game)
+	if err == nil {
+		t.Error("validate was supposed to find an error but did not")
+	}
+
+	cbs[1][1] = map[rune]bool{'%': true}
+	err = cbs.validate(game)
+	if err == nil {
+		t.Error("validate was supposed to find an error but did not")
+	}
+
+	// Test #2 - Invalid game state
+	game, ok = magnets.Deserialize("3x4:212,1202,122,2111,TTTBBBLRTLRB")
+	if !ok {
+		t.Errorf("Unable to deserialize board")
+	}
+
+	cbs = new(game)
+
+	err = cbs.validate(game)
+	if err != nil {
+		t.Error("Error validating CBS", err)
+	}
+
+	game.Guess.Set(0, 0, common.Negative)
+	game.Guess.Set(0, 1, common.Negative)
+
+	err = cbs.validate(game)
+	if err == nil {
+		t.Error("validate was supposed to find an error but did not")
+	}
+}
--- solver/solver.go
+++ solver/solver.go
@@ -1,93 +1,11 @@
 package solver
 
-// This package implements a constraints-based solver.
-//
-// The CBS solver holds a matrix representation that mimics the playing grid.
-//
-// It first finds whether there are any walls on the playing grid. If there are,
-// it marks those in its representation as being walls.
-//
-// Then it marks the rest of the cells as having all of the possibilities (positive,
-// negative, or neutral).
-//
-// As the solver works through the information it is given it eliminates possibilities
-// as it proves them non-possible. When the list of possibilities for a cell drops to
-// just one, the solver sets that in the Guess grid as a known entity.
-
 import (
 	"fmt"
 	"github.com/erikbryant/magnets/board"
 	"github.com/erikbryant/magnets/common"
 	"github.com/erikbryant/magnets/magnets"
 )
-
-// CBS is the constraint-based solver representation.
-type CBS [][]map[rune]bool
-
-var (
-	dirty = false
-)
-
-// new takes a game and returns a new, initialized constraint-based solver object for that game.
-func new(game magnets.Game) CBS {
-	cbs := make(CBS, game.Guess.Height())
-
-	for row := 0; row < game.Guess.Height(); row++ {
-		cbs[row] = make([]map[rune]bool, game.Guess.Width())
-		for col := 0; col < game.Guess.Width(); col++ {
-			cbs[row][col] = make(map[rune]bool)
-		}
-	}
-
-	for cell := range game.Guess.Cells() {
-		row, col := cell.Unpack()
-		r := game.Guess.Get(row, col)
-		if r == common.Wall {
-			cbs[row][col] = map[rune]bool{r: true}
-			continue
-		}
-		if r != common.Empty {
-			fmt.Printf("ERROR: %d, %d was already set to '%c'\n", row, col, r)
-			continue
-		}
-
-		// Each cell is a set of possibilities. At the start, each case is possible.
-		cbs[row][col][common.Positive] = true
-		cbs[row][col][common.Negative] = true
-		cbs[row][col][common.Neutral] = true
-	}
-
-	return cbs
-}
-
-// setFrame takes a coordinate and a polarity, sets that, and sets the other end
-// of the frame to correspond. This is different from the other implementations
-// in that it also keeps track of whether the board is dirty and updates the CBS.
-func (cbs CBS) setFrame(game magnets.Game, row, col int, r rune) {
-	rowEnd, colEnd := game.GetFrameEnd(row, col)
-
-	if r != game.Guess.Get(row, col) || common.Negate(r) != game.Guess.Get(rowEnd, colEnd) {
-		dirty = true
-	}
-
-	// Set this end of the frame.
-	game.Guess.Set(row, col, r)
-	cbs[row][col] = map[rune]bool{r: true}
-
-	// Set the other end of the frame.
-	game.Guess.Set(rowEnd, colEnd, common.Negate(r))
-	cbs[rowEnd][colEnd] = map[rune]bool{common.Negate(r): true}
-}
-
-// unsetPossibility removes the given rune from the CBS' list of potential
-// cell values.
-func (cbs CBS) unsetPossibility(row, col int, r rune) {
-	if val, ok := cbs[row][col][r]; ok && val {
-		dirty = true
-	}
-
-	delete(cbs[row][col], r)
-}
 
 // justOne iterates through all empty cells. For any that have just one
 // possibility left in the cbs, it sets that frame.
@@ -106,70 +24,6 @@ func (cbs CBS) justOne(game magnets.Game) {
 
 		cbs.setFrame(game, row, col, r)
 	}
-}
-
-// rowNeeds calculates how many of a given polarity are still needed in order
-// to be complete.
-func rowNeeds(game magnets.Game, row int, r rune) int {
-	needs := game.CountRow(row, r)
-	has := game.Guess.CountRow(row, r)
-	return needs - has
-}
-
-// colNeeds calculates how many of a given polarity are still needed in order
-// to be complete.
-func colNeeds(game magnets.Game, col int, r rune) int {
-	needs := game.CountCol(col, r)
-	has := game.Guess.CountCol(col, r)
-	return needs - has
-}
-
-// rowHasSpaceForTotal counts how many *possible* locations are present for the
-// given polarity. This includes cells that have already been solved.
-func (cbs CBS) rowHasSpaceForTotal(game magnets.Game, row int, r rune) int {
-	count := 0
-	for col := 0; col < game.Guess.Width(); col++ {
-		if cbs[row][col][r] {
-			count++
-		}
-	}
-	return count
-}
-
-// colHasSpaceForTotal counts how many *possible* locations are present for the
-// given polarity. This includes cells that have already been solved.
-func (cbs CBS) colHasSpaceForTotal(game magnets.Game, col int, r rune) int {
-	count := 0
-	for row := 0; row < game.Guess.Height(); row++ {
-		if cbs[row][col][r] {
-			count++
-		}
-	}
-	return count
-}
-
-// rowHasSpaceForRemaining counts how many *possible* locations are present for
-// the given polarity. This DOES NOT INCLUDE cells that have already been solved.
-func (cbs CBS) rowHasSpaceForRemaining(game magnets.Game, row int, r rune) int {
-	count := 0
-	for col := 0; col < game.Guess.Width(); col++ {
-		if game.Guess.Get(row, col) == common.Empty && cbs[row][col][r] {
-			count++
-		}
-	}
-	return count
-}
-
-// colHasSpaceForRemaining counts how many *possible* locations are present for
-// the given polarity. This DOES NOT INCLUDE cells that have already been solved.
-func (cbs CBS) colHasSpaceForRemaining(game magnets.Game, col int, r rune) int {
-	count := 0
-	for row := 0; row < game.Guess.Height(); row++ {
-		if game.Guess.Get(row, col) == common.Empty && cbs[row][col][r] {
-			count++
-		}
-	}
-	return count
 }
 
 // satisfied looks at each row/col to see if there are exactly as many spaces
@@ -423,46 +277,6 @@ func (cbs CBS) zeroInCol(game magnets.Game) {
 	}
 }
 
-// validate returns an error if the game or the CBS is inconsistent.
-func (cbs CBS) validate(game magnets.Game) error {
-	if !game.Valid() {
-		return fmt.Errorf("Invalid game board state detected")
-	}
-
-	for cell := range game.Guess.Cells(common.Positive, common.Negative, common.Neutral) {
-		row, col := cell.Unpack()
-		r := game.Guess.Get(row, col)
-		// This is already solved, so the CBS should only have r in it.
-		for key := range cbs[row][col] {
-			if key != r {
-				return fmt.Errorf("ERROR: CBS %d, %d had extraneous '%c'", row, col, key)
-			}
-		}
-	}
-
-	// Validate that the CBS contains only expected possibilities.
-	for row := range cbs {
-		for col := range cbs[row] {
-			for key := range cbs[row][col] {
-				switch key {
-				case common.Positive:
-					continue
-				case common.Negative:
-					continue
-				case common.Neutral:
-					continue
-				case common.Wall:
-					continue
-				default:
-					return fmt.Errorf("ERROR: CBS %d, %d has unexpected '%c'", row, col, key)
-				}
-			}
-		}
-	}
-
-	return nil
-}
-
 // Solve attempts to find a solution for the game, or gives up if it cannot.
 func Solve(game magnets.Game) {
 	cbs := new(game)
@@ -496,6 +310,8 @@ func Solve(game magnets.Game) {
 
 		attempts++
 		if attempts > 500 {
+			fmt.Println("WARNING: unable to solve game after >500 attempts")
+			game.Print()
 			break
 		}
 	}
